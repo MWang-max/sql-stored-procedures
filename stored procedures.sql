78ba487630c889e68e249c57242c696b5dcaf4dd
MWang-max <mwang2769@gmail.com>
2026-06-24 01:22:47 -0400

    Update stored procedures.sql

--- stored procedures.sql
+++ stored procedures.sql
@@ -1,30 +1,34 @@
 CREATE PROCEDURE GetCustomerSummary(IN CountryName NVARCHAR(50))
 BEGIN
 
--- remove nulls
+    -- Step 1: remove nulls
 
-IF EXISTS(SELECT 1 FROM Customers WHERE Score IS NULL AND Country = CountryName)
-THEN
-    UPDATE Customers
-    SET Score = 0
-    WHERE Score IS NULL AND Country = CountryName;
-ELSE
-    SELECT 'No NULL scores found' AS ' ';
-END IF;
+    IF EXISTS(SELECT 1 FROM Customers WHERE Score IS NULL AND Country = CountryName)
+    THEN
+        UPDATE Customers
+        SET Score = 0
+        WHERE Score IS NULL AND Country = CountryName;
+    ELSE
+        SELECT 'No NULL scores found' AS ' ';
+    END IF;
 
-SELECT
-    COUNT(*) AS TotalCustomers,
-    AVG(Score) AS AvgScore
-FROM Customers
-WHERE Country = CountryName;
+    -- Step 2: generate summary reports
 
-SELECT
-    COUNT(OrderID) AS TotalOrders,
-    SUM(Sales) AS TotalSales
-FROM Orders as o 
-JOIN Customers as c
-ON c.CustomerID = o.CustomerID
-WHERE c.Country = CountryName;
+    SELECT
+        COUNT(*) AS TotalCustomers,
+        AVG(Score) AS AvgScore
+    FROM Customers
+    WHERE Country = CountryName;
+
+    -- Step 3: calculate total number of orders and total sales for a specific country
+
+    SELECT
+        COUNT(OrderID) AS TotalOrders,
+        SUM(Sales) AS TotalSales
+    FROM Orders as o 
+    JOIN Customers as c
+    ON c.CustomerID = o.CustomerID
+    WHERE c.Country = CountryName;
 
 END
 
@@ -32,4 +36,29 @@ CALL GetCustomerSummary('Germany')
 
 CALL GetCustomerSummary('USA')
 
-DROP Procedure GetCustomerSummary
+DROP PROCEDURE GetCustomerSummary
+
+CREATE TABLE EmployeeLogs (
+    LogID INT PRIMARY KEY,
+    EmployeeID INT,
+    LogDate DATE
+)
+
+CREATE TRIGGER trg_AfterInsertEmployee AFTER INSERT ON Employees
+FOR EACH ROW
+BEGIN
+    INSERT INTO EmployeeLogs (LogID, EmployeeID, LogDate)
+    VALUES(
+        EmployeeID,
+        GETDATE()
+    );
+END;
+
+SELECT * FROM EmployeeLogs
+
+INSERT INTO Employees 
+VALUES(7, '1988-01-12')
+
+DROP TABLE EmployeeLogs
+
+DROP TRIGGER trg_AfterInsertEmployee
